143c39c8c9cce2d1fc3297ac1e8f7fcd47059e39
irao12 <ivanrao@outlook.com>
2022-06-18 22:39:35 -0400

    implemented code to color a square black after the user hovers over it

--- script.js
+++ script.js
@@ -13,9 +13,26 @@ function setup(size){
         for (let j = 0; j < size; j++) {
             let currSquare = document.createElement("div");
             currSquare.classList.add("square");
+
+            if (j == size - 1) {
+                currSquare.classList.add("lastCol");
+            }
+
             currRow.appendChild(currSquare);
         }
+        if (i == size - 1) {
+            currRow.classList.add("lastRow");
+        }
+
+        canvas.appendChild(currRow);
     }
+
+    const squares = document.querySelectorAll(".square");
+    console.log(squares);
+    squares.forEach( square => square.addEventListener('mouseover', function (e){
+        e.target.style.backgroundColor = "black";
+        console.log(e.target.style.backgroundColor);
+    }));
 }
 
 setup(size);
